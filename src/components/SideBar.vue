b68eeec5a5ca1700c77a30da3d45f8f8a553c979
Luis Reyes <iscluisreyes@gmail.com>
2022-08-24 14:24:13 -0500

    Luis Reyes
    Configuración de SideBar

---
+++ src/components/SideBar.vue
@@ -0,0 +1,96 @@
+<template>
+    <!--BARRA DE NAVEGACIÓN | Luis Reyes-->
+    <nav>
+        <v-navigation-drawer v-model="drawer" darp app mini-variant mini-variant-width="80" class="">
+            <v-list>
+                <v-list-item class="mb-12">
+                    <v-list-item-content>
+                        <v-icon color="#3d83df" class="mb-2" large>fa-solid fa-notes-medical</v-icon>
+                        <!-- <v-list-item-subtitle aling="center">Aryy</v-list-item-subtitle> -->
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+            <v-list flat>
+                <v-list-item router to="/">
+                    <v-list-item-content>
+                        <v-icon color="#98a1ab" class="mb-2" large>fa-solid fa-calendar-days</v-icon>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item router to="/">
+                    <v-list-item-content>
+                        <v-icon color="#98a1ab" class="mb-2" large>fa-light fa-user-group</v-icon>
+                    </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item router to="/">
+                    <v-list-item-content>
+                        <v-icon color="#98a1ab" class="mb-2" large>fa-solid fa-comment-dots</v-icon>
+                    </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item router to="/">
+                    <v-list-item-content>
+                        <v-icon color="#98a1ab" class="mb-2" large>fa-solid fa-chart-line</v-icon>
+                    </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item router to="/">
+                    <v-list-item-content>
+                        <v-icon color="#98a1ab" class="mb-2" large>fa-solid fa-file-invoice</v-icon>
+                    </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item router to="/">
+                    <v-list-item-content>
+                        <v-icon color="#98a1ab" class="mb-2" large>fa-regular fa-paper-plane</v-icon>
+                    </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item router to="/">
+                    <v-list-item-content>
+                        <v-icon color="#98a1ab" class="mb-2" large>fa-duotone fa-calculator</v-icon>
+                    </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item router to="/">
+                    <v-list-item-content>
+                        <v-icon color="#98a1ab" class="mb-2" large>fa-solid fa-gear</v-icon>
+                    </v-list-item-content>
+                </v-list-item>
+
+                <v-divider></v-divider>
+            </v-list>
+
+            <v-list style="position:absolute; bottom: 0;" class="mb-2 ml-3" flat>
+                <v-list-item router to="/">
+                    <v-list-action>
+                        <v-icon>fa-solid fa-gear</v-icon>
+                    </v-list-action>
+                </v-list-item>
+
+                <v-list-item router to="/">
+                    <v-list-action>
+                        <v-icon >fa-solid fa-gear</v-icon>
+                    </v-list-action>
+                </v-list-item>
+
+                <v-list-item router to="/">
+                    <v-list-action>
+                        <v-icon>fa-solid fa-gear</v-icon>
+                    </v-list-action>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+    </nav>
+</template>
+
+
+
+
+<script>
+export default {
+    data: () => ({
+        drawer: true
+    })
+}
+</script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,24 @@
+import '@fortawesome/fontawesome-free/css/all.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import colors from 'vuetify/lib/util/colors'
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    icons: {
+        iconfont: 'md' || 'fa'
+    },
+    theme:{
+        thems:{
+            dark:{
+                background: colors.pink.lighten5
+            },
+            light:{
+                background: colors.shades.white
+            }
+        }
+
+    }
 });
